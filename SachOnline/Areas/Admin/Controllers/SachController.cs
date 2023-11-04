8a7f290cec61f68f46c55eadea8edc62ea4a0d94
doangiabao645 <2124802010145@student.tdmu.edu.vn>
2023-11-04 11:06:43 +0700

    Lap5

---
+++ SachOnline/Areas/Admin/Controllers/SachController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SachOnline.Areas.Admin.Controllers
+{
+    public class SachController : Controller
+    {
+        // GET: Admin/Sach
+        public ActionResult Index()
+        {
+            return View();
+        }
+        [HttpGet]
+        public ActionResult Login()
+        {
+            return View();
+        }
+    }
+}
---
+++ SachOnline/Controllers/SachOnlineController.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration.Provider;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using SachOnline.Models;
+using PagedList;
+using PagedList.Mvc;
+
+namespace SachOnline.Controllers
+{
+    public class SachOnlineController : Controller
+    {
+        private string connection;
+        private dbSachOnlineDataContext data;
+
+        public SachOnlineController()
+        {
+            // Khởi tạo chuỗi kết nối
+            connection = "Data Source=LAPTOP-O5R6LPBI;Initial Catalog=SachOnline;Integrated Security=True";
+            data = new dbSachOnlineDataContext(connection);
+        }
+        public ActionResult NavPartial()
+        {
+            return PartialView();
+        }
+        private List<SACH> LaySachMoi(int count)
+        {
+            return data.SACHes.OrderByDescending(a => a.NgayCapNhat).Take(count).ToList();
+        }
+        // GET: SachOnline
+        public ActionResult Index()
+        {
+            //Lay 6 quyen sach moi
+            var listSachMoi = LaySachMoi(6);
+            return View(listSachMoi);
+        }
+        // GET: SachOnline
+
+        public ActionResult SachBanNhieuPartial()
+        {
+            // Truy vấn cơ sở dữ liệu để lấy danh sách sách bán nhiều nhất.
+            var listSachBanNhieu = data.SACHes.OrderByDescending(a => a.SoLuongBan).Take(6).ToList();
+
+            // Trả về PartialView chứa danh sách sách bán nhiều nhất.
+            return PartialView("SachBanNhieuPartial", listSachBanNhieu);
+        }
+
+        public ActionResult ChuDePartial()
+        {
+            var listChuDe = from cd in data.CHUDEs select cd;
+            return PartialView(listChuDe);
+        }
+        public ActionResult NhaXuatBanPartial()
+        {
+            var listNhaXuatBan = from cd in data.NHAXUATBANs select cd;
+            return PartialView(listNhaXuatBan);
+        }
+
+        // Lab3
+        public ActionResult SachTheoChuDe(int id, int? page)
+        {
+            int pageSize = 3;
+            int pageNumber = page ?? 1;
+
+            var sach = data.SACHes.Where(s => s.MaCD == id)
+                                  .OrderByDescending(s => s.NgayCapNhat)
+                                  .ToPagedList(pageNumber, pageSize);
+
+            ViewBag.Id = id;
+
+            return View(sach);
+        }
+        public ActionResult SachTheoNhaXuatBan(int id)
+        {
+            var sach = from s in data.SACHes where s.MaNXB == id select s;
+            return View(sach);
+        }
+        public ActionResult ChiTietSach(int id)
+        {
+            var sach = from s in data.SACHes
+                       where s.MaSach == id
+                       select s;
+            return View(sach.Single());
+        }
+        public ActionResult LoginLogout()
+        {
+            return PartialView("LoginLogoutPartial");
+        }
+
+    }
+}
